89f58d7b243360e8ab5e82817151feac1ecc6fe3
rafaelsaretto98 <rafaelcorazzasaretto@gmail.com>
2026-06-07 21:49:46 -0300

    Create jogos-firebase.js

---
+++ jogos-firebase.js
@@ -0,0 +1,47 @@
+import { db } from './firebase.js';
+
+import {
+    collection,
+    addDoc,
+    getDocs,
+    updateDoc,
+    doc
+}
+from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+export async function carregarJogos(){
+
+    const snapshot =
+        await getDocs(
+            collection(db, 'jogos')
+        );
+
+    return snapshot.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+    }));
+}
+
+export async function adicionarJogo(jogo){
+
+    return await addDoc(
+        collection(db, 'jogos'),
+        jogo
+    );
+}
+
+export async function atualizarResultado(
+    id,
+    golsA,
+    golsB
+){
+
+    await updateDoc(
+        doc(db, 'jogos', id),
+        {
+            golsA,
+            golsB,
+            encerrado: true
+        }
+    );
+}
