84d8631b9c0ac5f64dc7044732e1b1bf2cc9351a
rafaelsaretto98 <rafaelcorazzasaretto@gmail.com>
2026-06-07 21:26:06 -0300

    Update firestore.js

--- firestore.js
+++ firestore.js
@@ -2,17 +2,30 @@ import { db } from './firebase.js';
 
 import {
     collection,
-    addDoc
+    addDoc,
+    getDocs,
+    query,
+    where
 }
 from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+export async function participanteExiste(nome){
+
+    const q = query(
+        collection(db, 'participantes'),
+        where('nome', '==', nome)
+    );
+
+    const resultado =
+        await getDocs(q);
+
+    return !resultado.empty;
+}
 
 export async function salvarParticipante(dados){
 
     return await addDoc(
-        collection(
-            db,
-            'participantes'
-        ),
+        collection(db, 'participantes'),
         dados
     );
 }
