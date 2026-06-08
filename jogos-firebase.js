e2770cf4466deaf44cc8ff10d15b801e6d9562ab
rafaelsaretto98 <rafaelcorazzasaretto@gmail.com>
2026-06-08 20:54:11 -0300

    Update jogos-firebase.js

--- jogos-firebase.js
+++ jogos-firebase.js
@@ -8,7 +8,7 @@ import {
     deleteDoc,
     doc
 }
-from "...firebase-firestore.js";
+from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
 export async function carregarJogos(){
 
