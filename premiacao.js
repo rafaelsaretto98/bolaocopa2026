0098501178137b745846b1db76e2762cadde32f1
rafaelsaretto98 <rafaelcorazzasaretto@gmail.com>
2026-06-09 13:50:38 -0300

    Create premiacao.js

---
+++ premiacao.js
@@ -0,0 +1,34 @@
+import {
+    carregarParticipantes
+}
+from './participantes.js';
+
+async function carregarPremiacao(){
+
+    const participantes =
+        await carregarParticipantes();
+
+    const total =
+        participantes.length;
+
+    const premio =
+        total * 20;
+
+    document.getElementById(
+        'totalParticipantes'
+    ).textContent = total;
+
+    document.getElementById(
+        'valorPremio'
+    ).textContent =
+        premio.toLocaleString(
+            'pt-BR',
+            {
+                style:'currency',
+                currency:'BRL'
+            }
+        );
+
+}
+
+carregarPremiacao();
