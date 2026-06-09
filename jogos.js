10ef085efd549e394e5d2c628a2e3e53cebf0f26
rafaelsaretto98 <rafaelcorazzasaretto@gmail.com>
2026-06-09 09:37:50 -0300

    Update jogos.js

--- jogos.js
+++ jogos.js
@@ -24,6 +24,8 @@ async function iniciar(){
     
     });
 
+    let dataAtual = '';
+
     const lista =
         document.getElementById(
             'listaJogos'
@@ -31,57 +33,36 @@ async function iniciar(){
 
     lista.innerHTML = '';
 
-    jogos.forEach(jogo => {
+   jogos.forEach(jogo => {
 
-        const card =
+    if(dataAtual !== jogo.data){
+
+        dataAtual = jogo.data;
+
+        const tituloData =
             document.createElement('div');
 
-        card.className =
-            'regras-card';
+        tituloData.className =
+            'header-card';
 
-        card.innerHTML = `
-            <h3>
-                ${jogo.timeA}
-                x
-                ${jogo.timeB}
-            </h3>
+        tituloData.style.marginTop = '20px';
 
-            <p>
-                Grupo ${jogo.grupo}
-            </p>
-
-            <p>
-                📅 ${jogo.data}
-                às
-                ${jogo.horario}
-            </p>
-
-            <p>
-                🏟 ${jogo.estadio}
-            </p>
-
-            ${
-                jogo.encerrado
-                ?
-                `
-                <h2>
-                    ${jogo.golsA}
-                    x
-                    ${jogo.golsB}
-                </h2>
-                `
-                :
-                `
-                <strong>
-                    Aguardando resultado
-                </strong>
-                `
-            }
+        tituloData.innerHTML = `
+            <h2>
+                📅 ${formatarData(jogo.data)}
+            </h2>
         `;
 
-        lista.appendChild(card);
+        lista.appendChild(tituloData);
 
-    });
+    }
+
+function formatarData(data){
+
+    const partes =
+        data.split('-');
+
+    return `${partes[2]}/${partes[1]}/${partes[0]}`;
 
 }
 
