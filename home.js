c94fc2191536d7ecbcd057de6a75ab8ff2a8ee7f
rafaelsaretto98 <rafaelcorazzasaretto@gmail.com>
2026-06-11 16:00:45 -0300

    Update home.js

--- home.js
+++ home.js
@@ -8,6 +8,11 @@ import {
 }
 from './participantes-firebase.js';
 
+import {
+    carregarParticipantesRanking
+}
+from './ranking-firebase.js';
+
 
 
 const dashboard =
@@ -188,5 +193,122 @@ function formatarData(data){
         data.split('-');
 
     return `${partes[2]}/${partes[1]}`;
+
+}
+
+const resultadosOficiais =
+    JSON.parse(
+        localStorage.getItem(
+            'resultadosOficiais'
+        )
+    ) || {};
+
+function calcularPontos(palpiteUsuario){
+
+    let pontos = 0;
+
+    for(const grupo in resultadosOficiais){
+
+        if(palpiteUsuario[grupo]){
+
+            for(
+                let p of [
+                    '1º',
+                    '2º',
+                    '3º',
+                    '4º'
+                ]
+            ){
+
+                const escolha =
+                    palpiteUsuario[grupo][p];
+
+                const oficial =
+                    resultadosOficiais[grupo][p];
+
+                if(
+                    escolha &&
+                    oficial &&
+                    escolha === oficial
+                ){
+
+                    pontos++;
+
+                }
+
+            }
+
+        }
+
+    }
+
+    return pontos;
+
+}
+
+async function carregarTopRanking(){
+
+    const participantes =
+        await carregarParticipantesRanking();
+
+    participantes.forEach(p => {
+
+        p.pontuacao =
+            calcularPontos(
+                p.palpites
+            );
+
+    });
+
+    participantes.sort(
+        (a,b) =>
+            b.pontuacao -
+            a.pontuacao
+    );
+
+    const top3 =
+        participantes.slice(0,3);
+
+    const container =
+        document.getElementById(
+            'topRanking'
+        );
+
+    container.innerHTML = '';
+
+    const medalhas = [
+        '🥇',
+        '🥈',
+        '🥉'
+    ];
+
+    top3.forEach(
+        (p,index) => {
+
+            const div =
+                document.createElement(
+                    'div'
+                );
+
+            div.style.marginBottom =
+                '10px';
+
+            div.innerHTML = `
+                <strong>
+                    ${medalhas[index]}
+                    ${p.nome}
+                </strong>
+
+                <br>
+
+                ${p.pontuacao} pts
+            `;
+
+            container.appendChild(
+                div
+            );
+
+        }
+    );
 
 }
