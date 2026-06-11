156c6bc7ac5417706e32478b18b50134e9e56197
rafaelsaretto98 <rafaelcorazzasaretto@gmail.com>
2026-06-11 15:21:43 -0300

    Create home.js

---
+++ home.js
@@ -0,0 +1,48 @@
+const dashboard =
+    document.getElementById(
+        'dashboard'
+    );
+
+dashboard.innerHTML = `
+
+    <div class="regras-card">
+        <h2>
+            👥 Participantes
+        </h2>
+
+        <h1 id="totalParticipantes">
+            ...
+        </h1>
+    </div>
+
+    <div class="regras-card">
+        <h2>
+            💰 Premiação Atual
+        </h2>
+
+        <h1 id="premiacao">
+            ...
+        </h1>
+    </div>
+
+    <div class="regras-card">
+        <h2>
+            🔥 Próximos Jogos
+        </h2>
+
+        <div id="proximosJogos">
+            Carregando...
+        </div>
+    </div>
+
+    <div class="regras-card">
+        <h2>
+            🏆 Top 3
+        </h2>
+
+        <div id="topRanking">
+            Carregando...
+        </div>
+    </div>
+
+`;
