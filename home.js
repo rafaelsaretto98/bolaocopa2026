18425bf56f7792072ef7d50ee1b6c616ba17d96c
rafaelsaretto98 <rafaelcorazzasaretto@gmail.com>
2026-06-11 15:37:02 -0300

    Update home.js

--- home.js
+++ home.js
@@ -56,3 +56,33 @@ dashboard.innerHTML = `
     </div>
 
 `;
+
+async function carregarResumoBolao(){
+
+    const participantes =
+        await carregarParticipantes();
+
+    const total =
+        participantes.length;
+
+    const premio =
+        total * 20;
+
+    document.getElementById(
+        'totalParticipantes'
+    ).textContent = total;
+
+    document.getElementById(
+        'premiacao'
+    ).textContent =
+        premio.toLocaleString(
+            'pt-BR',
+            {
+                style:'currency',
+                currency:'BRL'
+            }
+        );
+
+}
+
+carregarResumoBolao();
