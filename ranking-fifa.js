c27d8af12c0d51944e44bbfb328fb92cde04d4f5
rafaelsaretto98 <rafaelcorazzasaretto@gmail.com>
2026-06-15 19:47:40 -0300

    Create ranking-fifa.js

---
+++ ranking-fifa.js
@@ -0,0 +1,14 @@
+export const rankingFifa = {
+
+    "Argentina": 1,
+    "França": 2,
+    "Espanha": 3,
+    "Inglaterra": 4,
+    "Brasil": 5,
+    "Portugal": 7,
+    "Alemanha": 10,
+    "Croácia": 11,
+    "Marrocos": 12,
+    "Uruguai": 13
+
+};
