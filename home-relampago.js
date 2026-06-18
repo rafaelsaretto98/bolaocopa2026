3eb2d2a852f8b90957354aa4b320d9344cc61acd
rafaelsaretto98 <rafaelcorazzasaretto@gmail.com>
2026-06-18 15:40:07 -0300

    Create home-relampago.js

---
+++ home-relampago.js
@@ -0,0 +1,43 @@
+export const boloesRelampago = [
+
+    {
+        id: 1,
+
+        encerrado: true,
+
+        timeA: 'Brasil',
+
+        timeB: 'Marrocos',
+
+        bandeiraA: '🇧🇷',
+
+        bandeiraB: '🇲🇦',
+
+        placar: '1 × 1',
+
+        vencedor: false,
+
+        mensagem: 'Nenhum participante acertou o placar.'
+    },
+
+    {
+        id: 2,
+
+        encerrado: false,
+
+        timeA: 'Brasil',
+
+        timeB: 'Haiti',
+
+        bandeiraA: '🇧🇷',
+
+        bandeiraB: '🇭🇹',
+
+        data: '19/06/2026',
+
+        horario: '20:00',
+
+        textoData: 'Sexta-feira • 19/06'
+    }
+
+];
