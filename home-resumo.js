ef5fe0582203203d45a4f85f70a0de36162c4759
rafaelsaretto98 <rafaelcorazzasaretto@gmail.com>
2026-06-18 14:40:33 -0300

    Update home-resumo.js

--- home-resumo.js
+++ home-resumo.js
@@ -1,5 +1,31 @@
+import {
+    carregarParticipantes
+}
+from './participantes-firebase.js';
+
 export async function carregarResumoBolao(){
 
-   ...
+    const participantes =
+        await carregarParticipantes();
 
+    const total =
+        participantes.length;
+
+    const premio =
+        total * 20;
+
+    document.getElementById(
+        'totalParticipantes'
+    ).textContent = total;
+
+    document.getElementById(
+        'premiacao'
+    ).textContent =
+        premio.toLocaleString(
+            'pt-BR',
+            {
+                style:'currency',
+                currency:'BRL'
+            }
+        );
 }
