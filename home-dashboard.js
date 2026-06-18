668aa492adcb216b05d571c5b3941664e62ce945
rafaelsaretto98 <rafaelcorazzasaretto@gmail.com>
2026-06-18 14:17:07 -0300

    Update home-dashboard.js

--- home-dashboard.js
+++ home-dashboard.js
@@ -4,3 +4,13 @@ document.getElementById('dashboard');
 dashboard.innerHTML = `
 ...
 `;
+
+export function criarDashboard(){
+
+    const dashboard =
+        document.getElementById('dashboard');
+
+    dashboard.innerHTML = `
+        ...
+    `;
+}
