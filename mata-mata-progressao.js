353ce05f611a7c70a345716b84bfed2fd8e38416
rafaelsaretto98 <rafaelcorazzasaretto@gmail.com>
2026-06-19 15:44:48 -0300

    Update mata-mata-progressao.js

--- mata-mata-progressao.js
+++ mata-mata-progressao.js
@@ -15,3 +15,49 @@ export function atualizarOitavas(jogos16Avos, jogosOitavas){
     });
 
 }
+
+export function atualizarQuartas(jogosOitavas, jogosQuartas){
+
+    jogosQuartas.forEach((jogo,index)=>{
+
+        const jogo1 = jogosOitavas[index * 2];
+
+        const jogo2 = jogosOitavas[index * 2 + 1];
+
+        jogo.timeA =
+            jogo1.vencedor;
+
+        jogo.timeB =
+            jogo2.vencedor;
+
+    });
+
+}
+
+export function atualizarSemifinais(jogosQuartas, jogosSemifinais){
+
+    jogosSemifinais.forEach((jogo,index)=>{
+
+        const jogo1 = jogosQuartas[index * 2];
+
+        const jogo2 = jogosQuartas[index * 2 + 1];
+
+        jogo.timeA =
+            jogo1.vencedor;
+
+        jogo.timeB =
+            jogo2.vencedor;
+
+    });
+
+}
+
+export function atualizarFinal(jogosSemifinais, jogoFinal){
+
+    jogoFinal.timeA =
+        jogosSemifinais[0].vencedor;
+
+    jogoFinal.timeB =
+        jogosSemifinais[1].vencedor;
+
+}
