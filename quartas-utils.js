60364642d3f31081b3f5090583e7d0e3367042ee
rafaelsaretto98 <rafaelcorazzasaretto@gmail.com>
2026-06-19 15:36:27 -0300

    Create quartas-utils.js

---
+++ quartas-utils.js
@@ -0,0 +1,91 @@
+export function gerarQuartas(jogosOitavas){
+
+    return [
+
+        {
+            id: 1,
+            fase: 'Quartas',
+
+            jogoA: jogosOitavas[0],
+            jogoB: jogosOitavas[1],
+
+            timeA: null,
+            timeB: null,
+
+            golsA: null,
+            golsB: null,
+
+            penaltisA: null,
+            penaltisB: null,
+
+            vencedor: null,
+
+            encerrado: false
+        },
+
+        {
+            id: 2,
+            fase: 'Quartas',
+
+            jogoA: jogosOitavas[2],
+            jogoB: jogosOitavas[3],
+
+            timeA: null,
+            timeB: null,
+
+            golsA: null,
+            golsB: null,
+
+            penaltisA: null,
+            penaltisB: null,
+
+            vencedor: null,
+
+            encerrado: false
+        },
+
+        {
+            id: 3,
+            fase: 'Quartas',
+
+            jogoA: jogosOitavas[4],
+            jogoB: jogosOitavas[5],
+
+            timeA: null,
+            timeB: null,
+
+            golsA: null,
+            golsB: null,
+
+            penaltisA: null,
+            penaltisB: null,
+
+            vencedor: null,
+
+            encerrado: false
+        },
+
+        {
+            id: 4,
+            fase: 'Quartas',
+
+            jogoA: jogosOitavas[6],
+            jogoB: jogosOitavas[7],
+
+            timeA: null,
+            timeB: null,
+
+            golsA: null,
+            golsB: null,
+
+            penaltisA: null,
+            penaltisB: null,
+
+            vencedor: null,
+
+            encerrado: false
+        }
+
+    ];
+
+}
