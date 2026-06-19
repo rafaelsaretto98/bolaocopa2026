b35b40ce67728e457e38797b9c91e9713fb972ae
rafaelsaretto98 <rafaelcorazzasaretto@gmail.com>
2026-06-19 15:35:24 -0300

    Create oitavas-utils.js

---
+++ oitavas-utils.js
@@ -0,0 +1,127 @@
+export function gerarOitavas(jogos16Avos){
+
+    return [
+
+        {
+            id: 1,
+            fase: 'Oitavas',
+            jogoA: jogos16Avos[0],
+            jogoB: jogos16Avos[1],
+            timeA: null,
+            timeB: null,
+            golsA: null,
+            golsB: null,
+            penaltisA: null,
+            penaltisB: null,
+            vencedor: null,
+            encerrado: false
+        },
+
+        {
+            id: 2,
+            fase: 'Oitavas',
+            jogoA: jogos16Avos[2],
+            jogoB: jogos16Avos[3],
+            timeA: null,
+            timeB: null,
+            golsA: null,
+            golsB: null,
+            penaltisA: null,
+            penaltisB: null,
+            vencedor: null,
+            encerrado: false
+        },
+
+        {
+            id: 3,
+            fase: 'Oitavas',
+            jogoA: jogos16Avos[4],
+            jogoB: jogos16Avos[5],
+            timeA: null,
+            timeB: null,
+            golsA: null,
+            golsB: null,
+            penaltisA: null,
+            penaltisB: null,
+            vencedor: null,
+            encerrado: false
+        },
+
+        {
+            id: 4,
+            fase: 'Oitavas',
+            jogoA: jogos16Avos[6],
+            jogoB: jogos16Avos[7],
+            timeA: null,
+            timeB: null,
+            golsA: null,
+            golsB: null,
+            penaltisA: null,
+            penaltisB: null,
+            vencedor: null,
+            encerrado: false
+        },
+
+        {
+            id: 5,
+            fase: 'Oitavas',
+            jogoA: jogos16Avos[8],
+            jogoB: jogos16Avos[9],
+            timeA: null,
+            timeB: null,
+            golsA: null,
+            golsB: null,
+            penaltisA: null,
+            penaltisB: null,
+            vencedor: null,
+            encerrado: false
+        },
+
+        {
+            id: 6,
+            fase: 'Oitavas',
+            jogoA: jogos16Avos[10],
+            jogoB: jogos16Avos[11],
+            timeA: null,
+            timeB: null,
+            golsA: null,
+            golsB: null,
+            penaltisA: null,
+            penaltisB: null,
+            vencedor: null,
+            encerrado: false
+        },
+
+        {
+            id: 7,
+            fase: 'Oitavas',
+            jogoA: jogos16Avos[12],
+            jogoB: jogos16Avos[13],
+            timeA: null,
+            timeB: null,
+            golsA: null,
+            golsB: null,
+            penaltisA: null,
+            penaltisB: null,
+            vencedor: null,
+            encerrado: false
+        },
+
+        {
+            id: 8,
+            fase: 'Oitavas',
+            jogoA: jogos16Avos[14],
+            jogoB: jogos16Avos[15],
+            timeA: null,
+            timeB: null,
+            golsA: null,
+            golsB: null,
+            penaltisA: null,
+            penaltisB: null,
+            vencedor: null,
+            encerrado: false
+        }
+
+    ];
+
+}
