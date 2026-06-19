648653502ba45d90a3cb0ed135fa260474746ae3
rafaelsaretto98 <rafaelcorazzasaretto@gmail.com>
2026-06-19 13:49:53 -0300

    Create mata-mata-config.js

---
+++ mata-mata-config.js
@@ -0,0 +1,99 @@
+export const confrontosOitavas = [
+
+{
+    id:1,
+    mandante:"1E",
+    visitante:"3ABCDF"
+},
+
+{
+    id:2,
+    mandante:"1I",
+    visitante:"3CDFGH"
+},
+
+{
+    id:3,
+    mandante:"2A",
+    visitante:"2B"
+},
+
+{
+    id:4,
+    mandante:"1F",
+    visitante:"2C"
+},
+
+{
+    id:5,
+    mandante:"2K",
+    visitante:"2L"
+},
+
+{
+    id:6,
+    mandante:"1H",
+    visitante:"2J"
+},
+
+{
+    id:7,
+    mandante:"1D",
+    visitante:"3BEFIJ"
+},
+
+{
+    id:8,
+    mandante:"1G",
+    visitante:"3AEHIJ"
+},
+
+{
+    id:9,
+    mandante:"1C",
+    visitante:"2F"
+},
+
+{
+    id:10,
+    mandante:"2E",
+    visitante:"2I"
+},
+
+{
+    id:11,
+    mandante:"1A",
+    visitante:"3CEFHI"
+},
+
+{
+    id:12,
+    mandante:"1L",
+    visitante:"3EHIJK"
+},
+
+{
+    id:13,
+    mandante:"1J",
+    visitante:"2H"
+},
+
+{
+    id:14,
+    mandante:"2D",
+    visitante:"2G"
+},
+
+{
+    id:15,
+    mandante:"1B",
+    visitante:"3EFGIJ"
+},
+
+{
+    id:16,
+    mandante:"1K",
+    visitante:"3DEIJL"
+}
+
+];
