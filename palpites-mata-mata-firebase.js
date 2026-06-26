0b9bebb0e0de34694289078845c27721426cbbad
rafaelsaretto98 <rafaelcorazzasaretto@gmail.com>
2026-06-26 09:22:25 -0300

    Create palpites-mata-mata-firebase.js

---
+++ palpites-mata-mata-firebase.js
@@ -0,0 +1,77 @@
+import { db }
+from "./firebase.js";
+
+import {
+    collection,
+    query,
+    where,
+    getDocs,
+    updateDoc,
+    doc
+}
+from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+export async function carregarParticipante(nome){
+
+    const q =
+        query(
+            collection(db,"participantes"),
+            where("nome","==",nome)
+        );
+
+    const snapshot =
+        await getDocs(q);
+
+    if(snapshot.empty){
+
+        return null;
+
+    }
+
+    return {
+
+        id: snapshot.docs[0].id,
+
+        ...snapshot.docs[0].data()
+
+    };
+
+}
+
+export async function salvarPalpitesMataMata(
+
+    nome,
+
+    palpites
+
+){
+
+    const participante =
+        await carregarParticipante(nome);
+
+    if(!participante){
+
+        throw new Error(
+            "Participante não encontrado."
+        );
+
+    }
+
+    await updateDoc(
+
+        doc(
+            db,
+            "participantes",
+            participante.id
+        ),
+
+        {
+
+            palpitesMataMata:
+                palpites
+
+        }
+
+    );
+
+}
