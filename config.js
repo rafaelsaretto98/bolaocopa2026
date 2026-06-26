b166d37577b7edbc35e805fb32e3c64cad5a2909
rafaelsaretto98 <rafaelcorazzasaretto@gmail.com>
2026-06-26 18:33:52 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,35 @@
+export const GRUPOS = [
+
+"A","B","C","D",
+
+"E","F","G","H",
+
+"I","J","K","L"
+
+];
+
+export const FASES = [
+
+"16-avos",
+
+"oitavas",
+
+"quartas",
+
+"semifinal",
+
+"final"
+
+];
+
+export const POSICOES = [
+
+"1º",
+
+"2º",
+
+"3º",
+
+"4º"
+
+];
