a73f43eb134f64e79f6c8021c04c905686b3221b
rafaelsaretto98 <rafaelcorazzasaretto@gmail.com>
2026-06-28 20:55:55 -0300

    Create agenda-mata-mata.js

---
+++ agenda-mata-mata.js
@@ -0,0 +1,147 @@
+export const agendaMataMata = {
+
+    "16A-1":{
+
+        data:"",
+        horario:"",
+        cidade:"",
+        estadio:""
+
+    },
+
+    "16A-2":{
+
+        data:"",
+        horario:"",
+        cidade:"",
+        estadio:""
+
+    },
+
+    "16A-3":{
+
+        data:"",
+        horario:"",
+        cidade:"",
+        estadio:""
+
+    },
+
+    "16A-4":{
+
+        data:"",
+        horario:"",
+        cidade:"",
+        estadio:""
+
+    },
+
+    "16A-5":{
+
+        data:"",
+        horario:"",
+        cidade:"",
+        estadio:""
+
+    },
+
+    "16A-6":{
+
+        data:"",
+        horario:"",
+        cidade:"",
+        estadio:""
+
+    },
+
+    "16A-7":{
+
+        data:"",
+        horario:"",
+        cidade:"",
+        estadio:""
+
+    },
+
+    "16A-8":{
+
+        data:"",
+        horario:"",
+        cidade:"",
+        estadio:""
+
+    },
+
+    "16A-9":{
+
+        data:"",
+        horario:"",
+        cidade:"",
+        estadio:""
+
+    },
+
+    "16A-10":{
+
+        data:"",
+        horario:"",
+        cidade:"",
+        estadio:""
+
+    },
+
+    "16A-11":{
+
+        data:"",
+        horario:"",
+        cidade:"",
+        estadio:""
+
+    },
+
+    "16A-12":{
+
+        data:"",
+        horario:"",
+        cidade:"",
+        estadio:""
+
+    },
+
+    "16A-13":{
+
+        data:"",
+        horario:"",
+        cidade:"",
+        estadio:""
+
+    },
+
+    "16A-14":{
+
+        data:"",
+        horario:"",
+        cidade:"",
+        estadio:""
+
+    },
+
+    "16A-15":{
+
+        data:"",
+        horario:"",
+        cidade:"",
+        estadio:""
+
+    },
+
+    "16A-16":{
+
+        data:"",
+        horario:"",
+        cidade:"",
+        estadio:""
+
+    }
+
+};
