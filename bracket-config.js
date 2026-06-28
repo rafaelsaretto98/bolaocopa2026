f2f23abebea258faa447b6cfd25ea9deb90815df
rafaelsaretto98 <rafaelcorazzasaretto@gmail.com>
2026-06-28 09:25:38 -0300

    Create bracket-config.js

---
+++ bracket-config.js
@@ -0,0 +1,67 @@
+export const BRACKET = {
+
+    ladoEsquerdo:{
+
+        dezesseis:[
+            "16A-1",
+            "16A-2",
+            "16A-3",
+            "16A-4",
+            "16A-5",
+            "16A-6",
+            "16A-7",
+            "16A-8"
+        ],
+
+        oitavas:[
+            "OIT-1",
+            "OIT-2",
+            "OIT-3",
+            "OIT-4"
+        ],
+
+        quartas:[
+            "QUA-1",
+            "QUA-2"
+        ],
+
+        semi:[
+            "SEMI-1"
+        ]
+
+    },
+
+    ladoDireito:{
+
+        dezesseis:[
+            "16A-9",
+            "16A-10",
+            "16A-11",
+            "16A-12",
+            "16A-13",
+            "16A-14",
+            "16A-15",
+            "16A-16"
+        ],
+
+        oitavas:[
+            "OIT-5",
+            "OIT-6",
+            "OIT-7",
+            "OIT-8"
+        ],
+
+        quartas:[
+            "QUA-3",
+            "QUA-4"
+        ],
+
+        semi:[
+            "SEMI-2"
+        ]
+
+    },
+
+    final:"FINAL-1"
+
+};
