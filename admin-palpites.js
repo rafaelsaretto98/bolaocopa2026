75fd091624e35d3245b0a2b20d2d2513075d0f54
rafaelsaretto98 <rafaelcorazzasaretto@gmail.com>
2026-06-29 00:29:52 -0300

    Update admin-palpites.js

--- admin-palpites.js
+++ admin-palpites.js
@@ -1,4 +1,9 @@
 import {
+    carregarJogosMataMata
+}
+from "./mata-mata-firebase.js";
+
+import {
 
     criarNavbar
 
@@ -21,10 +26,131 @@ document.getElementById(
 async function iniciar(){
 
     const participantes =
-
         await carregarParticipantes();
+        
+    const jogos =
 
-    console.log(participantes);
+    await carregarJogosMataMata();
+
+        console.log(jogos);
+
+    const select =
+        document.getElementById(
+            "participante"
+        );
+
+    select.innerHTML =
+        '<option value="">Selecione um participante</option>';
+
+    participantes
+
+        .sort((a,b)=>
+
+            a.nome.localeCompare(b.nome)
+
+        )
+
+        .forEach(participante=>{
+
+            const option =
+                document.createElement("option");
+
+            option.value =
+                participante.nome;
+
+            option.textContent =
+                participante.nome;
+
+            select.appendChild(option);
+
+        });
+
+        select.addEventListener(
+
+    "change",
+
+    ()=>{
+
+        desenharJogos(
+
+            jogos,
+
+            select.value
+
+        );
+
+    }
+
+);
+
+}
+
+function desenharJogos(
+
+    jogos,
+
+    participante
+
+){
+
+    const lista =
+        document.getElementById(
+            "listaJogos"
+        );
+
+    lista.innerHTML = "";
+
+    jogos.forEach(jogo=>{
+
+        const nomeTimeA =
+            jogo.timeA?.time || "A definir";
+
+        const nomeTimeB =
+            jogo.timeB?.time || "A definir";
+
+        const card =
+            document.createElement("div");
+
+        card.className =
+            "regras-card";
+
+        card.innerHTML = `
+
+<h3>
+
+${nomeTimeA}
+
+x
+
+${nomeTimeB}
+
+</h3>
+
+<button
+class="btn-time"
+data-jogo="${jogo.id}"
+data-time="${nomeTimeA}"
+>
+
+${nomeTimeA}
+
+</button>
+
+<button
+class="btn-time"
+data-jogo="${jogo.id}"
+data-time="${nomeTimeB}"
+>
+
+${nomeTimeB}
+
+</button>
+
+`;
+
+        lista.appendChild(card);
+
+    });
 
 }
 
