81d31bacb821ed302a7d7baf0b47e54621d01753
rafaelsaretto98 <rafaelcorazzasaretto@gmail.com>
2026-06-29 01:00:55 -0300

    Update participantes-firebase.js

--- participantes-firebase.js
+++ participantes-firebase.js
@@ -4,7 +4,9 @@ import {
     collection,
     getDocs,
     doc,
-    updateDoc
+    updateDoc,
+    query,
+    where
 }
 from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
@@ -43,5 +45,48 @@ export async function atualizarParticipante(participante){
         dados
 
     );
+
+}
+
+import {
+    query,
+    where
+}
+from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+export async function carregarParticipante(nome){
+
+    const q =
+        query(
+
+            collection(
+                db,
+                "participantes"
+            ),
+
+            where(
+                "nome",
+                "==",
+                nome
+            )
+
+        );
+
+    const snapshot =
+        await getDocs(q);
+
+    if(snapshot.empty){
+
+        return null;
+
+    }
+
+    return {
+
+        id: snapshot.docs[0].id,
+
+        ...snapshot.docs[0].data()
+
+    };
 
 }
