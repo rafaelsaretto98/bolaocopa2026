a28ca3a50b75f17a864a3f1f69c2da3de8328ef4
rafaelsaretto98 <rafaelcorazzasaretto@gmail.com>
2026-07-01 20:17:35 -0300

    Create mata-mata-v2.js

---
+++ mata-mata-v2.js
@@ -0,0 +1,23 @@
+import {
+
+    criarNavbar
+
+}
+from "./navbar.js";
+
+import {
+
+    desenharChaveamento
+
+}
+from "./mata-mata-v2-layout.js";
+
+document.getElementById(
+
+    "navbar"
+
+).innerHTML =
+
+    criarNavbar("");
+
+desenharChaveamento();
