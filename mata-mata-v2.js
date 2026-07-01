58e8fd06044548f8a62d738bc50eaa49030e707a
rafaelsaretto98 <rafaelcorazzasaretto@gmail.com>
2026-07-01 20:19:19 -0300

    Update mata-mata-v2.js

--- mata-mata-v2.js
+++ mata-mata-v2.js
@@ -1,23 +1,33 @@
 import {
-
     criarNavbar
-
 }
 from "./navbar.js";
 
 import {
+    carregarJogosMataMata
+}
+from "./mata-mata-firebase.js";
 
+import {
     desenharChaveamento
-
 }
 from "./mata-mata-v2-layout.js";
 
 document.getElementById(
-
     "navbar"
-
 ).innerHTML =
+    criarNavbar("mata-mata");
 
-    criarNavbar("");
+async function iniciar(){
 
-desenharChaveamento();
+    const jogos =
+
+        await carregarJogosMataMata();
+
+    console.log(jogos);
+
+    desenharChaveamento(jogos);
+
+}
+
+iniciar();
