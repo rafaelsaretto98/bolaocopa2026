574f7731c3751644de0379b63f69d0a1a2e6edf2
rafaelsaretto98 <rafaelcorazzasaretto@gmail.com>
2026-07-01 20:41:52 -0300

    Update mata-mata-v2-layout.js

--- mata-mata-v2-layout.js
+++ mata-mata-v2-layout.js
@@ -9,6 +9,43 @@ export function desenharChaveamento(jogos){
 
     container.innerHTML = "";
 
+        const jogos16 =
+
+        jogos
+            .filter(j => j.fase === "16-avos")
+            .sort((a,b)=>a.ordem-b.ordem);
+
+            const coluna16 =
+        document.createElement("div");
+
+    coluna16.className =
+        "coluna-fase";
+
+    coluna16.innerHTML = `
+
+            <h2 class="titulo-fase">
+            
+            16 Avos
+            
+            </h2>
+            
+            `;
+        jogos16.forEach(jogo=>{
+
+        coluna16.appendChild(
+
+            criarCardJogo(jogo)
+
+        );
+
+    });
+
+        container.appendChild(
+        coluna16
+    );
+
+    
+
     const fases = [
 
         "16 Avos",
@@ -48,5 +85,51 @@ ${fase}
         );
 
     });
+
+}
+
+function criarCardJogo(jogo){
+
+    const card =
+        document.createElement("div");
+
+    card.className =
+        "card-jogo";
+
+    const timeA =
+        jogo.timeA?.time || "A definir";
+
+    const timeB =
+        jogo.timeB?.time || "A definir";
+
+    card.innerHTML = `
+
+<div class="linha-time">
+
+    <img
+        class="bandeira-mini"
+        src="img/band_${timeA}.png"
+        onerror="this.src='img/band_placeholder.png'"
+    >
+
+    <span>${timeA}</span>
+
+</div>
+
+<div class="linha-time">
+
+    <img
+        class="bandeira-mini"
+        src="img/band_${timeB}.png"
+        onerror="this.src='img/band_placeholder.png'"
+    >
+
+    <span>${timeB}</span>
+
+</div>
+
+`;
+
+    return card;
 
 }
