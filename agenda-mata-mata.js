07ea5026475d28d3cee6a8b510ddafd69fcc2ca8
rafaelsaretto98 <rafaelcorazzasaretto@gmail.com>
2026-07-04 09:10:13 -0300

    Update agenda-mata-mata.js

--- agenda-mata-mata.js
+++ agenda-mata-mata.js
@@ -1,147 +1,179 @@
 export const agendaMataMata = {
 
     "16A-1":{
-
         data:"2026-06-29",
         horario:"17:30",
         cidade:"Foxborough",
         estadio:"Gillette Stadium"
-
     },
 
     "16A-2":{
-
         data:"2026-06-30",
         horario:"18:00",
         cidade:"East Rutherford",
         estadio:"MetLife Stadium"
-
     },
 
     "16A-3":{
-
         data:"2026-06-28",
         horario:"16:00",
         cidade:"Inglewood",
         estadio:"SoFi Stadium"
-
     },
 
     "16A-4":{
-
         data:"2026-06-29",
         horario:"22:00",
         cidade:"Monterrey",
         estadio:"Estadio BBVA"
-
     },
 
     "16A-5":{
-
         data:"2026-07-02",
         horario:"20:00",
         cidade:"Toronto",
         estadio:"BMO Field"
-
     },
 
     "16A-6":{
-
         data:"2026-07-02",
         horario:"16:00",
         cidade:"Inglewood",
         estadio:"SoFi Stadium"
-
     },
 
     "16A-7":{
-
         data:"2026-07-01",
         horario:"21:00",
         cidade:"Santa Clara",
         estadio:"Levi's Stadium"
-
     },
 
     "16A-8":{
-
         data:"2026-07-01",
         horario:"17:00",
         cidade:"Seattle",
         estadio:"Lumen Field"
-
     },
 
     "16A-9":{
-
         data:"2026-06-29",
         horario:"14:00",
         cidade:"Houston",
         estadio:"NRG Stadium"
-
     },
 
     "16A-10":{
-
         data:"2026-06-30",
         horario:"14:00",
         cidade:"Arlington",
         estadio:"AT&T Stadium"
-
     },
 
     "16A-11":{
-
         data:"2026-06-30",
         horario:"22:00",
         cidade:"Cidade do México",
         estadio:"Estadio Azteca"
-
     },
 
     "16A-12":{
-
         data:"2026-07-01",
         horario:"13:00",
         cidade:"Atlanta",
         estadio:"Mercedes-Benz Stadium"
-
     },
 
     "16A-13":{
-
         data:"2026-07-03",
         horario:"19:00",
         cidade:"Miami",
         estadio:"Hard Rock Stadium"
-
     },
 
     "16A-14":{
-
         data:"2026-07-03",
         horario:"15:00",
         cidade:"Arlington",
         estadio:"AT&T Stadium"
-
     },
 
     "16A-15":{
-
         data:"2026-07-03",
         horario:"00:00",
         cidade:"Vancouver",
         estadio:"BC Place"
-
     },
 
     "16A-16":{
-
         data:"2026-07-03",
         horario:"22:30",
         cidade:"Kansas City",
         estadio:"Arrowhead Stadium"
+    },
 
+    "OIT-1":{
+
+        data:"2026-07-04",
+        horario:"18:00",
+        cidade:"",
+        estadio:""
+    },
+
+    "OIT-2":{
+
+        data:"2026-07-04",
+        horario:"22:30",
+        cidade:"",
+        estadio:""
+    },
+
+    "OIT-3":{
+
+        data:"2026-07-06",
+        horario:"16:00",
+        cidade:"",
+        estadio:""
+    },
+
+    "OIT-4":{
+
+        data:"2026-07-06",
+        horario:"21:00",
+        cidade:"",
+        estadio:""
+    },
+
+        "OIT-5":{
+
+        data:"2026-07-05",
+        horario:"17:00",
+        cidade:"",
+        estadio:""
+    },
+
+        "OIT-6":{
+
+        data:"2026-07-05",
+        horario:"21:00",
+        cidade:"",
+        estadio:""
+    },
+
+        "OIT-7":{
+
+        data:"2026-07-07",
+        horario:"13:00",
+        cidade:"",
+        estadio:""
+    },
+
+        "OIT-8":{
+
+        data:"2026-07-07",
+        horario:"17:00",
+        cidade:"",
+        estadio:""
     }
 
 };
